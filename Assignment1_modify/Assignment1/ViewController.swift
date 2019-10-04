33f98afc05a3dbf67b8eee6c8874bf5e97831b73
aimhigh53 <37101427+aimhigh53@users.noreply.github.com>
2019-10-05 00:03:40 +0900

    Add files via upload
    not yet..

---
+++ Assignment1_modify/Assignment1/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by ins25k on 04/10/2019.
+//  Copyright © 2019 ins25k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var likeLable: UILabel!
+    @IBOutlet weak var emptyheart: UIImageView!
+    @IBOutlet weak var Heartbtn: UIButton!
+    
+    let emptyHeartimg = UIImage(named: "emptyheart")
+    let fullHeartimg = UIImage(named: "fullheart")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeHeart(_ sender: Any) {
+    
+    //실패했습니다 ㅠㅠ 다음주 안에 채워넣겠습니다
+          
+      }
+    }
+    
+
+
